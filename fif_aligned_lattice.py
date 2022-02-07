da0692b8a9418e2cc26a00c6c73f4fad95fe1b32
sm-egan <shannon.egan@mail.mcgill.ca>
2022-02-06 19:56:21 -0800

    Moved some plotting functions from fif_aligned_lattice and fif_plotting

--- fif_aligned_lattice.py
+++ fif_aligned_lattice.py
@@ -10,7 +10,9 @@ import scipy.sparse as sp
 from scipy.sparse.linalg import eigsh
 from numpy.linalg import eigh
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import os.path
+import time
 
 R = 2
 paulis = np.array([
@@ -107,7 +109,7 @@ def make_lattice(maxL):
     return mesh[selection]
 
 nn1 = np.array([[1,0],[1,1],[0,1],[-1,0],[-1,-1],[0,-1]])
-nn2 = np.array([[2,-1],[1,2],[-1,1],[-2,-1],[-1,-2],[1,-1]])
+nn2 = np.array([[2,1],[1,2],[-1,1],[-2,-1],[-1,-2],[1,-1]])
 
 def find_nn(lattice, deg = 1):
     nnarr = np.full((len(lattice), 6), None)
@@ -161,7 +163,7 @@ def H_hop(t):
     #print(indptr)
     data = np.array([-t]*len(indlist))
     
-    print('H_hop indlist has {} elements'.format(len(indlist)))
+    #print('H_hop indlist has {} elements'.format(len(indlist)))
     halfmat = sp.csr_matrix((data, indlist, indptr), shape = (N, N))
     return sp.kron(paulis[3], halfmat + halfmat.transpose())
 
@@ -180,7 +182,7 @@ def H_sc1(Delta1, plot = False):
         indptr.append(indptr[-1] + len(nninds))
         
     data = np.array([-Delta1]*len(indlist))
-    print('H_sc1 indlist has {} elements'.format(len(indlist)))
+    #print('H_sc1 indlist has {} elements'.format(len(indlist)))
     halfmat = sp.csr_matrix((data, indlist, indptr), shape = (N, N))
     if plot:
         plt.pcolor(halfmat.toarray())
@@ -189,8 +191,15 @@ def H_sc1(Delta1, plot = False):
     if plot:
         plt.pcolor(halfmat.toarray())
         plt.show()
-    data2 = np.array([halfmat.toarray(), halfmat.getH().toarray()])
-    return sp.bsr_matrix((data2, np.array([1,0]), np.array([0,1,2])), shape=(2*N, 2*N))
+        
+    '''
+    Used this method to put blocks together before I learned about sp.bmat
+    bmat is 30-50% faster
+    
+    #data2 = np.array([halfmat.toarray(), halfmat.getH().toarray()])
+    #return sp.bsr_matrix((data2, np.array([1,0]), np.array([0,1,2])), shape=(2*N, 2*N))
+    '''
+    return sp.bmat([[None, halfmat], [halfmat.getH(), None]]).tocsr()
 
 def H_sc2(Delta2, plot = False):
     # Bonds with +i order param
@@ -209,7 +218,7 @@ def H_sc2(Delta2, plot = False):
         data = np.array([-Delta1]*len(indlist))
     
     data = np.array([-(Delta2*1j)]*len(indlist))
-    print('H_sc2 indlist has {} elements'.format(len(indlist)))
+    #print('H_sc2 indlist has {} elements'.format(len(indlist)))
     halfmat = sp.csr_matrix((data, indlist, indptr), shape = (N, N))
     if plot:
         plt.pcolor(np.imag(halfmat.toarray()))
@@ -218,8 +227,10 @@ def H_sc2(Delta2, plot = False):
     if plot:
         plt.pcolor(np.imag(halfmat.toarray()))
         plt.show()
-    data2 = np.array([halfmat.toarray(), halfmat.getH().toarray()])
-    return sp.bsr_matrix((data2, np.array([1,0]), np.array([0,1,2])), shape=(2*N, 2*N))
+        
+    #data2 = np.array([halfmat.toarray(), halfmat.getH().toarray()])
+    #return sp.bsr_matrix((data2, np.array([1,0]), np.array([0,1,2])), shape=(2*N, 2*N))
+    return sp.bmat([[None, halfmat], [halfmat.getH(), None]]).tocsr()
     
     # block = -(Delta2/2)*1j*paulis[1]
     # data = np.array([block]*len(indlist))
@@ -228,9 +239,64 @@ def H_sc2(Delta2, plot = False):
     # return halfmat + np.conj(halfmat.transpose())
 
 def H_fif(mu, t, Delta1, Delta2):
-    return H_diag(mu) + H_hop(t) + H_sc1(Delta1) + H_sc2(Delta2)
+    start = time.time()
+    sc1 = H_sc1(Delta1)
+    stop = time.time()
+    print('H_sc1 took {0:5f} seconds to build'.format(stop - start))
+    
+    start = time.time()
+    sc2 = H_sc2(Delta2)
+    stop = time.time()
+    print('H_sc2 too {0:5f} seconds to build'.format(stop-start))
+    return H_diag(mu) + H_hop(t) + sc1 + sc2
 
-def plot_grid(mesh, colour = None, grid = 'tri', title='', clims = None, savename = None, pointsize=None):
+def vortex_phase(vsite, vorticity, raw = True):
+    '''
+    Creates sparse matrix which can be multiplied by the Hamiltonian to give
+    phase winding on the order parameter
+
+    Parameters
+    ----------
+    vsite : int
+        Lattice site where vortex appears.
+    vorticity : int
+        Number of times the phase winds around the vortex.
+
+    Returns
+    -------
+    None.
+
+    '''
+    N = len(lattice)
+    bonds = np.arange(0,6)
+    phase1 = np.exp(1j*vorticity*pi/3*bonds)
+    phase2 = np.exp(1j*vorticity*pi*(1/3*bonds + 1/6))
+    
+    row1 = np.array([vsite]*6)
+    row2 = np.array([vsite]*6)
+    
+    col1 = lattnn1[vsite]
+    col2 = lattnn2[vsite]
+    
+    data1 = phase1 
+    data2 = phase2 
+    if not raw:
+        data1 -= 1
+        data2 -= 1
+    upper1 = sp.csr_matrix((data1, (row1, col1)), shape = (N, N))
+    upper1 = upper1 + upper1.transpose()
+    
+    upper2 = sp.csr_matrix((data2, (row2, col2)), shape = (N, N))
+    upper2 = upper2 + upper2.transpose()
+    
+    halfmat = upper1 + upper2
+    
+    return sp.bmat([[None, halfmat],[halfmat.getH(), None]]).tocsr()
+
+def find_origin():
+    return np.where(np.all(lattice == np.array([0,0]), axis = 1))[0][0]
+
+def plot_grid(mesh, colour = None, grid = 'tri', title='', clims = None, savename = None, pointsize = None, cmap = 'YlGnBu'):
     xlist = []
     ylist = []
     
@@ -255,12 +321,12 @@ def plot_grid(mesh, colour = None, grid = 'tri', title='', clims = None, savenam
     else:
         if clims == None:
             minmax = np.max(colour)
-            
-            plt.scatter(xlist, ylist, c=colour, cmap = 'YlGnBu', s=size, marker=mark, vmin=0, vmax=minmax)
+            plt.scatter(xlist, ylist, c = colour, cmap = cmap, s = size, marker = mark)
+            #plt.pcolormesh(xlist, ylist, colour, cmap = cmap, shading = 'nearest')
         else:
-            plt.scatter(xlist, ylist, c=colour, cmap = 'YlGnBu', s=size, marker=mark, vmin=clims[0], vmax=clims[1])
+            plt.scatter(xlist, ylist, c = colour, cmap = 'YlGnBu', s = size, marker = mark)
            
-    plt.colorbar().set_label('Amplitude', size = 'x-large')
+    plt.colorbar().set_label('Amplitude (a.u.)', size = 'x-large')
     plt.xticks(fontsize=13)
     plt.yticks(fontsize=13)
     plt.xlabel(r'$x$', size='xx-large')
@@ -279,13 +345,14 @@ def is_hermitian(A):
     
 if __name__ == '__main__':
     #plot_grid(lattice, pointsize = 500)
-    R = 32
-    k = 2**3
+    R = 24
+    k = 2**5
     t = 1
-    mu = 1
+    mu = 0.9
     Delta1 = 0.2
     Delta2 = 0.1
     use_sparse = False
+    vortex = True
     
     lattice = make_lattice(R)
     lattnn1 = find_nn(lattice)
@@ -293,29 +360,38 @@ if __name__ == '__main__':
     N = len(lattice)
     
     H = H_fif(t, mu, Delta1, Delta2)
-    title1 = r'$R =$ {0}, $N =$ {1}'.format(R, N)
+    vsite = find_origin()
+    if vortex:
+        V = vortex_phase(vsite, 1, False)
+        H = H + V*H
+    
+    #title1 = r'$R =$ {0}, $N =$ {1}, $\mu=$ {2}'.format(R, N, mu)
+    title1 = r'$N =$ {0}, $\mu=$ {1}'.format(N, mu)
     if use_sparse:
+        savename = 'fif_eigsh'
         evals, evecs = eigsh(H, k = k, which='SM')
         evals = np.sort(evals)
-        np.savez('fif_eigsh_R{0}_k{1}'.format(R, k), evals, evecs)
         title1 = title1 + r', $k =$ {}'.format(k)
     else:
         #Takes about 13 minutes for R = 32
+        savename = 'fif_eigh'
         evals, evecs = eigh(H.toarray())
-        np.savez('fif_eigh_R{0}'.format(R), evals, evecs)
+    if vortex:
+        savename = savename + '_vortex'
+    np.savez('data/' + savename + '_R{0}_mu{1:.2f}'.format(R, mu).replace('.',''), evals, evecs)
     
-    plt.plot(evals, 'o')
-    #plt.ylim(-0.5, 0.5)
-    plt.xlabel('Eigenstate index', size='x-large')
-    plt.ylabel('Energy', size='x-large')
-    plt.title(title1)
-    plt.show()
     
-    zero_evals = np.where(np.abs(evals) < 0.04)[0]
-    if len(zero_evals) < 12:
-        for ind in zero_evals:
-            state = evecs[:, ind][0:N]
-            amp = np.abs(state)
-            norm = np.sum(amp**2)
-            title2 = title1 + ', $E =$ {0:.2f}'.format(evals[ind])
-            plot_grid(lattice, colour = amp/np.sqrt(norm), pointsize=10, title = title2)
+    zero_evals = np.where(np.abs(evals) < 0.1)[0]
+
+    for ind in zero_evals:
+        state = evecs[:, ind][N:]
+        amp = np.abs(state)**2
+        norm = np.sum(amp**2)
+        title2 = title1 + ', $E =$ {0:.4f}'.format(evals[ind])
+        
+        if evals[ind] > -0.005:
+            plot_grid(lattice, colour = amp/norm, pointsize=12, title = title2, savename = savename + '_ind{}'.format(ind) + '.pdf')
+        else:
+            plot_grid(lattice, colour = amp/norm, pointsize=12, title = title2)
+    
+    
--- fif_plotting.py
+++ fif_plotting.py
@@ -9,9 +9,10 @@ from scipy.constants import pi
 import scipy.sparse as sp
 import matplotlib.pyplot as plt
 import os
-from fif_aligned_lattice import make_lattice
+from fif_aligned_lattice import make_lattice, plot_grid
 
 def get_ldos(evals, evecs, sites, dw, wrange = None):
+    N = len(evals)
     if wrange is None:
         emin = np.min(evals)
         emax = np.max(evals)
@@ -37,19 +38,54 @@ def get_ldos(evals, evecs, sites, dw, wrange = None):
     return wrange, np.array(ldos)
 
 if __name__ == '__main__':
-    infile = np.load('fif_eigh_R32.npz')
+    mu = 0.9
+    
+    filename = 'data/fif_eigh_vortex_R32_mu{0:.2f}'.format(mu).replace('.','') + '.npz'
+    infile = np.load(filename)
     evals = infile['arr_0']
     evecs = infile['arr_1']
+    
+    N = len(evals)//2
+    title1 = r'$N =$ {0}, $\mu=$ {1}'.format(N, mu)
+    savename = filename.split('/')[1].split('.npz')[0]
+    if 'vortex' in filename:
+        savename = savename + 'vortex'
     
     lattice = make_lattice(32)
     center = np.where(np.all(lattice == np.array([0,0]), axis = 1))[0][0]
     
-    wrange, ldos = get_ldos(evals, evecs, center + 20, 0.1)
+    wrangec, ldosc = get_ldos(evals, evecs, center, 0.1)
+    wrangee, ldose = get_ldos(evals, evecs, 0, 0.1)
     
-    plt.plot(wrange, ldos)
-    plt.xlabel()
-    #plt.xlim(-1,1)
+    fig, (axc, axe) = plt.subplots(ncols = 2, figsize = (10, 6))
+    axc.plot(wrangec, ldosc)
+    axc.set_xlabel(r'$\omega$', size = 'xx-large')
+    axc.set_ylabel(r'$\rho(\omega, \mathbf{r})$', size = 'xx-large')
+    axc.set_xlim(-6,6)
+    axc.set_title('LDOS at origin')
     
+    axe.plot(wrangee, ldose)
+    axe.set_xlim(-6, 6)
+    axe.set_title('LDOS at edge')
+    axe.set_xlabel(r'$\omega$', size = 'xx-large')
     
+    if 'vortex' in savename:
+        fig.suptitle('With vortex', size = 'xx-large')
+    else:
+        fig.suptitle('Without vortex', size = 'xx-large')
+    fig.tight_layout()
     
+    zero_evals = np.where(np.abs(evals) < 0.03)[0]
     
+    fig, ax = plt.subplots(figsize = (4,10))
+    ax.plot(np.arange(0, len(evals))[np.abs(evals) > 0.03], evals[np.abs(evals) > 0.03], 'o')
+    ax.plot(zero_evals, evals[zero_evals], 'x', c='r', markersize = 12, label = r'$|E| < 0.03$')
+    ax.set_xlim(3150, 3190)
+    ax.set_ylim(-0.15,0.15)
+    ax.set_xlabel('Eigenstate index', size='x-large')
+    ax.set_ylabel('Energy', size='xx-large')
+    ax.set_title(title1, size = 'x-large')
+    ax.legend(loc = 2, prop = {'size': 14})
+    plt.show()
+    
+    #if len(zero_evals) < 12:
